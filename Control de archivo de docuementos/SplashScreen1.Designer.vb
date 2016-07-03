799ae96608c7a94f9a465072515bf4bd685b2b2b
WeBob <jrconstantinor@gmail.com>
2016-07-02 22:46:34 -0700

    Add project files.

---
+++ Control de archivo de docuementos/SplashScreen1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SplashScreen1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ApplicationTitle = New System.Windows.Forms.Label()
+        Me.Copyright = New System.Windows.Forms.Label()
+        Me.Version = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'ApplicationTitle
+        '
+        Me.ApplicationTitle.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.ApplicationTitle.BackColor = System.Drawing.Color.Transparent
+        Me.ApplicationTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ApplicationTitle.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ApplicationTitle.Location = New System.Drawing.Point(272, 9)
+        Me.ApplicationTitle.Name = "ApplicationTitle"
+        Me.ApplicationTitle.Size = New System.Drawing.Size(394, 41)
+        Me.ApplicationTitle.TabIndex = 3
+        Me.ApplicationTitle.Text = "Application Title"
+        Me.ApplicationTitle.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'Copyright
+        '
+        Me.Copyright.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Copyright.BackColor = System.Drawing.Color.Transparent
+        Me.Copyright.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Copyright.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Copyright.Location = New System.Drawing.Point(524, 357)
+        Me.Copyright.Name = "Copyright"
+        Me.Copyright.Size = New System.Drawing.Size(241, 21)
+        Me.Copyright.TabIndex = 5
+        Me.Copyright.Text = "Copyright Todos los Derechos Reservados"
+        '
+        'Version
+        '
+        Me.Version.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Version.BackColor = System.Drawing.Color.Transparent
+        Me.Version.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Version.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Version.Location = New System.Drawing.Point(503, 266)
+        Me.Version.Name = "Version"
+        Me.Version.Size = New System.Drawing.Size(142, 22)
+        Me.Version.TabIndex = 4
+        Me.Version.Text = "Version {0}.{1:00}"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'SplashScreen1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Control_de_archivo_de_docuementos.My.Resources.Resources.Logo_Archivero
+        Me.ClientSize = New System.Drawing.Size(771, 379)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.ApplicationTitle)
+        Me.Controls.Add(Me.Copyright)
+        Me.Controls.Add(Me.Version)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "SplashScreen1"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ApplicationTitle As Label
+    Friend WithEvents Copyright As Label
+    Friend WithEvents Version As Label
+    Friend WithEvents Timer1 As Timer
+End Class
